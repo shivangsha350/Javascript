0c24b45a24b97ed09a75e08ad6a5fff4a54da2a9
shivang sharma <shivangsha350@gmail.com>
2026-03-22 00:45:21 +0000

    variables

---
+++ Basics/Variables.js
@@ -0,0 +1,11 @@
+const accountId = 06048 
+let accountName = "Shivang Sharma"
+let accountCity = "Jaipur"
+let accountState = "Rajasthan"
+
+console.table([accountId , accountName , accountCity , accountState]);
+
+/*
+Use always let apart from var 
+Because there is a scope issue in Var 
+*/
