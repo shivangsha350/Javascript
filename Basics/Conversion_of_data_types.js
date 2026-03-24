65d062f59cd3ab54fdb3deea56e2a4f2cc8809ec
shivang sharma <shivangsha350@gmail.com>
2026-03-24 11:11:38 +0000

    conversion

---
+++ Basics/Conversion_of_data_types.js
@@ -0,0 +1,28 @@
+// let Id = NaN          
+                            
+// console.log(typeof Id); 
+// console.log(Id);
+                
+                                        
+// let st = Number(Id)
+// console.log(typeof st);
+// console.log(st);
+
+
+//NaN ka typeof number aata h or string me NaN
+//undefined in string is NaN 
+//null ka typeof object   value in string is 0
+
+
+//***************************Operations************************************/
+
+// console.log("2" + 1 + 2);  //Ans is 212 (because if the strings comes
+// console.log(1 + 2 + "2"); //Ans is 32 first they make everyone as a string) 
+
+// console.log(true);      // It gives true
+// // console.log(true+);  //It gives an error bcz there is nthng after operand
+// console.log(+true);     //It gives 1 because it converts boolen to number
+
+// let a = 5
+// a++        //post increment operator
+// console.log(a);
